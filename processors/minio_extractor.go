061294fdae910a87c39e1be0496b3b54b3555a7e
cx-rotems <rotem.sheps@checkmarx.com>
2025-01-16 23:36:16 +0200

    change to 50

--- processors/minio_extractor.go
+++ processors/minio_extractor.go
@@ -16,7 +16,7 @@ func NewMinioExtractor() *MinioExtractor {
 
 func (me *MinioExtractor) Process(job types.Job) (types.Job, error) {
 	// Simulate extraction
-	for i := 1; i < 50; i++ {
+	for i := 1; i <= 50; i++ {
 		time.Sleep(100 * time.Millisecond) // simulate download from Minio
 		job.Results = append(job.Results, types.Result{ResultID: i, JobID: job.ID})
 	//	fmt.Printf("MinioExtractor: Downlaod result ID %d and job ID  %d\n",  job.Results[i].ResultID,  job.Results[i].JobID) 
